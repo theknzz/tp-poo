08660b2ed3fe0c104836aecbf6b1b6fd3e948f03
k n z z <joaquimpersonalmail@gmail.com>
2018-11-16 19:22:40 +0000

    Revert "comandos"
    This reverts commit dca2607b99f38e9ebfed932fb74091439647ab8e.

---
+++ tp-poo/Comandos.h
@@ -0,0 +1,17 @@
+#ifndef __COMANDOS__
+#define __COMANDOS__
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Comandos
+{
+public:
+	bool menu()const;
+
+};
+
+#endif
