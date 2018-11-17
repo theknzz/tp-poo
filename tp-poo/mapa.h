862b811214b3a90c446ebeb80d00e36bc7b2358b
k n z z <34898147+theknzz@users.noreply.github.com>
2018-11-17 21:41:23 +0000

    upload

--- tp-poo/mapa.h
+++ tp-poo/mapa.h
@@ -4,4 +4,4 @@
 #include "Consola.h"
 
 void mostra_mapa(int alt,int larg,vector<Celula>); // alt = NUMERO DE LINHAS DO MAPA ... larg = NUMERO DE COLUNAS DO MAPA
-vector<Celula> le_mapa(vector<Celula> cel);
+vector<Celula> le_mapa();
